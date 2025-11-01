62b92dc6ca4d1520ff02443bce91a6768905128f
phj1120 <hyeonj1998@naver.com>
2025-11-01 19:12:12 +0900

    승인 취소 공통 코드 추가

--- api/src/main/java/com/api/app/emum/PAY004.java
+++ api/src/main/java/com/api/app/emum/PAY004.java
@@ -12,6 +12,7 @@ public enum PAY004 implements CommonCode {
     PAYMENT("001", "결제", 1, "", ""),
     APPROVAL("002", "승인", 2, "", ""),
     NETWORK_CANCEL("003", "망취소", 3, "", ""),
+    CANCEL("004", "취소", 3, "", ""),
     ;
 
     private final String code;
